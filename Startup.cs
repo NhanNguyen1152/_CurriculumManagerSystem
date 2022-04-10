03786779a37e22212a18244124d1b225fa67c31c
NHAN NGUYEN THANH <46920791+NhanNguyen1152@users.noreply.github.com>
2022-04-10 23:20:22 +0700

    update startup

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace _CurriculumManagerSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<acomptec_lvthainhanContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

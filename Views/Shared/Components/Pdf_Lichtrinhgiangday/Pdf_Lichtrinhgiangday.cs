ea23db2a1803bf83a98a5399e4c9acb2b3c48856
NHAN NGUYEN THANH <46920791+NhanNguyen1152@users.noreply.github.com>
2022-06-04 23:17:32 +0700

    Update noidungchuongtrinh dao tao

---
+++ Views/Shared/Components/Pdf_Lichtrinhgiangday/Pdf_Lichtrinhgiangday.cs
@@ -0,0 +1,34 @@
+using System.Text.RegularExpressions;
+using System.Net.WebSockets;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _CurriculumManagerSystem.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace _CurriculumManagerSystem.Views.Shared.Components.Pdf_Lichtrinhgiangday
+{
+    public class Pdf_Lichtrinhgiangday : ViewComponent
+    {
+        private readonly acomptec_lvthainhanContext _context;
+
+        public Pdf_Lichtrinhgiangday(acomptec_lvthainhanContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {   
+            //sualaithanhchitietchuong
+            var result = _context.Chitiet_Chuongs.Include(m => m.Chitietmonhoc).ThenInclude(z => z.DeCuongchiTiet).Where(m => m.Chitietmonhoc.mahp == id);
+            return View(await result.ToListAsync());
+
+        }
+    }
+}

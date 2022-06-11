ec2b634d927b34b93c8897ef9f11827fbc4a4a10
NHAN NGUYEN THANH <46920791+NhanNguyen1152@users.noreply.github.com>
2022-06-12 01:52:20 +0700

    tum lum ver 3

--- Views/Shared/Components/Pdf_ctdt_kehoachgiangday/Pdf_ctdt_kehoachgiangday.cs
+++ Views/Shared/Components/Pdf_ctdt_kehoachgiangday/Pdf_ctdt_kehoachgiangday.cs
@@ -1,7 +1,33 @@
+using System.Net.WebSockets;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _CurriculumManagerSystem.Models;
+using Microsoft.AspNetCore.Http;
+
+
 namespace _CurriculumManagerSystem.Views.Shared.Components.Pdf_ctdt_kehoachgiangday
 {
-    public class Pdf_ctdt_kehoachgiangday
+    public class Pdf_ctdt_kehoachgiangday : ViewComponent
     {
-        
+        private readonly acomptec_lvthainhanContext _context;
+
+        public Pdf_ctdt_kehoachgiangday(acomptec_lvthainhanContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {   
+            var appDbContext = _context.Decuong_Chuongtrinhs.Include(d => d.DeCuongchiTiet).ThenInclude(d => d.Khoikienthuc).Where(d => d.ma_ctdt == id);
+            TempData["listData"] = appDbContext.ToList();
+            return View();
+        }
     }
 }
--- Views/Shared/Components/Pdf_ctdt_khoiluongkienthuc/Pdf_ctdt_khoiluongkienthuc.cs
+++ Views/Shared/Components/Pdf_ctdt_khoiluongkienthuc/Pdf_ctdt_khoiluongkienthuc.cs
@@ -1,7 +1,41 @@
+using System.Net.WebSockets;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _CurriculumManagerSystem.Models;
+using Microsoft.AspNetCore.Http;
+
 namespace _CurriculumManagerSystem.Views.Shared.Components.Pdf_ctdt_khoiluongkienthuc
 {
-    public class Pdf_ctdt_khoiluongkienthuc
+    public class Pdf_ctdt_khoiluongkienthuc : ViewComponent
     {
-        
+        private readonly acomptec_lvthainhanContext _context;
+
+        public Pdf_ctdt_khoiluongkienthuc(acomptec_lvthainhanContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {   
+            // var result = await _context.CLOPhuongphapdays.Include(c => c.Chuandaura_monhoc).Include(c => c.Phuongphap_Giangday).Where(m => m.Chuandaura_monhoc.mahp == HttpContext.Session.GetInt32("id_edit_dccht_after")).ToListAsync();
+            // return View<List<CLOPhuongphapday>>(result);
+            var appDbContext = _context.Decuong_Chuongtrinhs.Include(d => d.DeCuongchiTiet).ThenInclude(d => d.Khoikienthuc).Where(d => d.ma_ctdt == id);
+            TempData["listData"] = appDbContext.ToList();
+            return View();
+        }
     }
 }
+
+// public IActionResult getNoidungCTDT()
+//         {
+//             var appDbContext = _context.DeCuongchiTiets.Include(d => d.Khoikienthuc);
+//             TempData["listData"] = appDbContext.ToList();
+//             return View();
+//         }
